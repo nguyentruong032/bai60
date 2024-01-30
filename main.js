8b7c766b326edfe88b3a2561835992ecd3515e81
nguyentruong032 <thesucceeder032045@gmail.com>
2024-01-30 15:37:37 +0700

    add

--- main.js
+++ main.js
@@ -13,3 +13,11 @@ overlay.addEventListener("click", () => {
   menuButton.classList.toggle("open");
   overlay.classList.toggle("show");
 });
+var index = 0;
+hinh_anh = function () {
+  var arr_hinh = ["./img/0.jpg", "./img/1.jpg", "./img/2.jpg"];
+  document.getElementById("hinh").src = arr_hinh[index];
+  index++;
+  if (index >= arr_hinh.length) index = 0;
+};
+setInterval(hinh_anh, 1000);
