9aa28fd29a9fb58f9e1874d92a4f0f76fede24de
nguyentruong032 <thesucceeder032045@gmail.com>
2024-01-31 09:59:40 +0700

    add

--- main.js
+++ main.js
@@ -21,3 +21,26 @@ hinh_anh = function () {
   if (index >= arr_hinh.length) index = 0;
 };
 setInterval(hinh_anh, 1000);
+
+function validateForm() {
+  var x = document.getElementById("Email").value;
+  var y = document.getElementById("SDT").value;
+  var z = document.getElementById("MS").value;
+
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(x)) {
+    alert("Vui lòng nhập email hợp lệ.");
+    return false;
+  }
+  var teleregex = /^[0-9]{10}$/;
+  if (!teleregex.test(y)) {
+    alert("Vui lòng nhập đúng định dạng số điện thoại");
+    return false;
+  }
+  if (z.trim() === "") {
+    alert("vui lòng điền nội dung");
+    return false;
+  }
+  alert("Thông tin đã được gửi!");
+  return true;
+}
