52a28faca43c036aca8fb69301b38015715fbc76
nguyentruong032 <thesucceeder032045@gmail.com>
2024-02-09 05:17:22 +0700

    add

--- main.js
+++ main.js
@@ -44,3 +44,10 @@ function validateForm() {
   alert("Thông tin đã được gửi!");
   return true;
 }
+document.getElementById("backToTopBtn").addEventListener("click", function () {
+  // Sử dụng window.scrollTo để cuộn về đầu trang web
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Tạo hiệu ứng cuộn mượt nếu trình duyệt hỗ trợ
+  });
+});
